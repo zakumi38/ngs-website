3db81a3952685851e8469557a0dea43c1022e693
Sint Sint Hlaing <sints2001@gmail.com>
2022-08-17 17:52:03 +0630

    Update : responsive for gallery table

--- src/app/pages/gallery/gallery-list/gallery-table.jsx
+++ src/app/pages/gallery/gallery-list/gallery-table.jsx
@@ -70,7 +70,8 @@ const GalleryTable = () => {
     loadPosts();
   }, [currentPage,offSet]);
 
-  const Submit = () =>{
+  function Submit(value) {
+    SetCurrentPage(1);
     loadPosts();
   }
   
@@ -85,10 +86,10 @@ const GalleryTable = () => {
         margin: "5rem 0",
       }}
     >
-      <Grid item xs={3} sm={6}>
+      <Grid item  xs={12} sm={6}>
         <Typography variant="h5">Gallery List</Typography>
       </Grid>
-      <Grid item xs={9} sm={6}>
+      <Grid item mt={3} xs={12} sm={6}>
         <Stack
           direction="row"
           spacing={1}
@@ -106,7 +107,6 @@ const GalleryTable = () => {
           <OutlinedInput
             sx={{
               width: {
-                xs: "50%",
                 sm: "auto",
               },
             }}
